b9a30e62a57ea19f8d41efc10c0d4344c896cd9d
HoleInOneGolfer <benjamin.d.bowles@gmail.com>
2024-10-28 13:08:45 -0500

    templates

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["POST", "GET"])
 def index():
     return "Hello, World!"
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
