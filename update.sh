2a85009d6c8874ef0167ba989cb3c99868e536be
HoleInOneGolfer <benjamin.d.bowles@gmail.com>
2025-08-11 01:05:04 -0500

    update

--- update.sh
+++ update.sh
@@ -11,3 +11,6 @@ cd "$REPO_PATH" || exit 1
 git fetch origin main || exit 1
 git reset --hard origin/main || exit 1
 
+if [[ -f "$REPO_PATH/requirements.txt" ]]; then
+    pip install --upgrade -r "$REPO_PATH/requirements.txt" || exit 1
+fi
